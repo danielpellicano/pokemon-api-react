477d204f202dd6ff146beab4b134c2a592ccdbbb
Daniel Pellicano <danielwebmaster1@gmail.com>
2021-09-17 00:04:17 -0300

    initial config and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import { Header } from "./Components/Header";
+import GlobalStyle from "./globalStyles";
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,3 @@
+export function Header() {
+  return <h1>Pokemon Api</h1>;
+}
